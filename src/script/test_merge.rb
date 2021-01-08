2bed24557a732661b627520aa9420ae00a0c734f
Seo-4d696b75 <s.kaoru509@gmail.com>
2021-01-08 23:23:11 +0900

    [update] script

--- src/script/test_merge.rb
+++ src/script/test_merge.rb
@@ -1,4 +1,5 @@
 # check there is no contradiction between a new dataset and old one
+load("src/script/diff.rb")
 load("src/script/utils.rb")
 require "minitest/autorun"
 
@@ -70,60 +71,6 @@ class MergeTest < Minitest::Test
     data["lines"].each { |l| @lines[l["id"]] = l }
 
     @log = "## detected diff from `extra` branch  \n\n"
-  end
-
-  def normalize_value(key, value, station_map)
-    if key == "lines"
-      # Array of Int
-      return value.sort
-    elsif key == "station_list"
-      # code => id
-      value.each do |item|
-        item["id"] = station_map[item["code"]]["id"]
-      end
-      return value
-    else
-      return value
-    end
-  end
-
-  def format_md(value, key = nil, station_map = nil)
-    if key == "polyline_list"
-      return "`{..data..}`"
-    elsif key == "station_list"
-      value.map! do |e|
-        s = station_map[e.delete("id")]
-        name = s["name"]
-        if n = s["numbering"]
-          next "#{name}(#{n.join("/")})"
-        else
-          next name
-        end
-      end
-    elsif key == "line" || key == "station"
-      return "#{value["name"]}(#{value["code"]})"
-    end
-    if value.kind_of?(Array) || value.kind_of?(Hash)
-      return "`#{JSON.dump(value)}`"
-    elsif value.kind_of?(Numeric) || value.kind_of?(String) || value == true || value == false
-      return value.to_s
-    elsif value == nil
-      return "null"
-    end
-    raise "unexpected type #{value} #{value.class}"
-  end
-
-  def check_diff(tag, id, old, current, fields)
-    fields.each do |key|
-      next if IGNORE.include?(key)
-      old_value = normalize_value(key, old[key], @old_station_map)
-      new_value = normalize_value(key, current[key], @station_map)
-      if old_value != new_value
-        old_value = format_md(old_value, key, @old_station_map)
-        new_value = format_md(new_value, key, @station_map)
-        @log << "- **#{tag}** id:#{id} name:#{current["name"]} #{key}:#{old_value}=>#{new_value}\n"
-      end
-    end
   end
 
   def test_id
