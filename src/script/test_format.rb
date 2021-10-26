2ce7a603ac3d5d4ad69c1dd74a084f113686ff89
Seo-4d696b75 <s.kaoru509@gmail.com>
2021-10-26 18:46:44 +0900

    [fix] format test script
    -branch 'feature/update`
    - target: main and extra dataset

--- src/script/test_format.rb
+++ src/script/test_format.rb
@@ -1,9 +1,15 @@
 require "minitest/autorun"
 load("src/script/format.rb")
+require "optparse"
+
+opt = OptionParser.new
+opt.on("-s", "--src VALUE") { |v| SRC = v }
+opt.parse!(ARGV)
+ARGV.clear()
 
 class CSVFormatTest < FormatTest
   def setup()
-    data = read_json("out/data.json")
+    data = read_json(SRC)
     @stations = data["stations"]
     @lines = data["lines"]
     self.check_init()
