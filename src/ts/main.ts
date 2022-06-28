3ae6bf80fb3f4430da75a886fa67fec81978cc37
Seo-4d696b75 <s.kaoru509@gmail.com>
2022-06-28 21:41:16 +0900

    追加：delaunay.schema.json

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,8 +3,9 @@ import { readCsvSafe, readJsonSafe } from "./io";
 import { csvLine, jsonLineList } from "./model/line";
 import { csvStation, jsonStation, jsonStationList } from "./model/station";
 import glob from "glob";
-import {writeFileSync} from "fs"
+import { writeFileSync } from "fs"
 import { jsonLineDetail } from "./model/lineDetail";
+import { jsonDelaunayList } from "./model/delaunay";
 
 // station.json
 writeFileSync("out/schema/station.schema.json", JSON.stringify(jsonStationList, undefined, 2))
@@ -12,3 +13,5 @@ writeFileSync("out/schema/station.schema.json", JSON.stringify(jsonStationList, 
 writeFileSync("out/schema/line.schema.json", JSON.stringify(jsonLineList, undefined, 2))
 // line/*.json
 writeFileSync("out/schema/line_detail.schema.json", JSON.stringify(jsonLineDetail, undefined, 2))
+// delaunay.json
+writeFileSync("out/schema/delaunay.schema.json", JSON.stringify(jsonDelaunayList, undefined, 2))
