ff0fdaf62c6743750bd868a0ccffaa485979d447
k_senda <k_senda@yumemi.co.jp>
2024-01-23 22:35:57 +0900

    feat: extract news

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,11 @@
 import axios from "axios"
 import { JSDOM } from "jsdom"
 
-async function getNewsPath(dom: JSDOM) {
+process.env.TZ = "Asia/Tokyo"
+
+async function getNewsPath() {
+  const body = (await axios.get<string>("https://ekimemo.com/news?page=8")).data
+  const dom = new JSDOM(body)
   const list = dom.window.document.querySelectorAll(".news-list > .news-item")
   const paths: string[] = []
   for (const e of list) {
@@ -18,9 +22,35 @@ async function getNewsPath(dom: JSDOM) {
   return paths
 }
 
-(async () => {
-  const body = (await axios.get<string>("https://ekimemo.com/news?page=8")).data
+function getUpdateDate(dom: JSDOM, publish: Date): Date {
+  const list = dom.window.document.querySelectorAll(".news-body > p")
+  for (const e of list) {
+    const m = e.textContent?.match(/(?<month>[0-9]{1,2})\/(?<day>[0-9]{1,2})に以下の内容にて駅情報の更新を予定/)
+    if (m && m.groups) {
+      const month = parseInt(m.groups["month"])
+      const day = parseInt(m.groups["day"])
+      const date = new Date(publish.getFullYear(), month, day)
+      while (date.getTime() < publish.getDate()) {
+        date.setFullYear(date.getFullYear() + 1)
+      }
+      return date
+    }
+  }
+  throw Error("update date not found")
+}
+
+async function processNewsItem(path: string) {
+  const body = (await axios.get<string>(`https://ekimemo.com${path}`)).data
   const dom = new JSDOM(body)
-  const paths = await getNewsPath(dom)
-  console.log(paths.length)
+  const publish = new Date(dom.window.document.querySelector(".news-status > .news-publish-time")?.getAttribute("datetime")!)
+  const update = getUpdateDate(dom, publish)
+  const newsBody = dom.window.document.querySelector(".news-body")?.innerHTML
+  console.log(publish, update)
+}
+
+(async () => {
+  const paths = await getNewsPath()
+  for (const path of paths) {
+    await processNewsItem(path)
+  }
 })()
