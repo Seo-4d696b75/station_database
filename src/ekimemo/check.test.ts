fdd85664e88c18e5863e68a4ed265560cc585e87
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-08-23 00:38:57 +0900

    feat: 検査スクリプトを高速化
    JSDOMは遅いので柔軟性は落ちるが正規表現に移行

--- src/ekimemo/check.test.ts
+++ src/ekimemo/check.test.ts
@@ -1,5 +1,5 @@
 import { JSONSchemaType } from 'ajv';
-import { JSDOM } from "jsdom";
+import { readFile } from "fs/promises";
 import { readCsvSafe, readJsonSafe } from '../ts/io';
 import { kanaName, stationLineName } from '../ts/model/common';
 import { csvPrefecture } from '../ts/model/prefecture';
@@ -101,50 +101,46 @@ describe("駅メモとの差分を検査", () => {
   const stations = readCsvSafe("src/ekimemo/station.csv", csvEkimemo)
   const set = new Set(stations.map(s => s.name))
 
-  describe("駅", () => {
-    const data = readCsvSafe("src/station.csv", csvStation).filter(s => !s.extra)
+  describe("駅データの確認", () => {
     const ekimemo_codes = new Map(stations.map(r => [r.code, r.ekimemo]))
+    const data = readCsvSafe("src/station.csv", csvStation)
+      .filter(s => !s.extra)
+      .map(s => ({ ...s, ekimemo: ekimemo_codes.get(s.code)!! }))
 
     const pref_list = readCsvSafe("src/check/prefecture.csv", csvPrefecture)
     const prefectures = new Map(pref_list.map(p => [p.name, p.code]) as [string, number][])
 
-    test.each(data)("%s", async (s) => {
-      const ekimemo = ekimemo_codes.get(s.code)!!
+    test.concurrent.each(data)("$name code:$code html:$ekimemo", async (s) => {
+      const html = (await readFile(`src/ekimemo/station/${s.ekimemo}.html`)).toString()
 
-      // https://github.com/jsdom/jsdom/issues/2583#issuecomment-559520814
-      const dom = await JSDOM.fromFile(`src/ekimemo/station/${ekimemo}.html`)
-      const pref = dom.window.document.querySelector(".station-name > .pref")?.textContent
-      const kana = dom.window.document.querySelector(".station-name > .kana")?.textContent
-      const path = dom.window.document.querySelector("img.station-map-img")?.getAttribute("src")
-      if (!pref || !kana || !path) throw Error("station data not found ")
-
+      const m = html.match(/<div class="station-name">.*?<div class="pref">(?<pref>.+?)<\/div>.*?<div class="kana">(?<kana>.+?)<\/div>/ms)
+      if (!m) throw Error("station data not found ")
+      const pref = m.groups?.["pref"]!!
+      const kana = m.groups?.["kana"]!!
       const pref_code = prefectures.get(pref)
-
       expect(s.prefecture).toBe(pref_code)
       expect(s.name_kana).toBe(kana)
 
-      const m = path.match(/\/static\/(?<lng>[\d.]+),(?<lat>[\d.]+),\d+/)
-      if (!m) throw Error("lat/lng not found " + ekimemo)
-      const lat = parseFloat(m.groups?.["lat"]!!).toFixed(6)
-      const lng = parseFloat(m.groups?.["lng"]!!).toFixed(6)
+      const path = html.match(/<img\s+src="https:\/\/mfmap.com\/styles\/ekimemo-app\/static\/(?<lng>[\d.]+),(?<lat>[\d.]+),\d+/ms)
+      if (!path) throw Error("lat/lng not found " + s.ekimemo)
+      const lat = parseFloat(path.groups?.["lat"]!!).toFixed(6)
+      const lng = parseFloat(path.groups?.["lng"]!!).toFixed(6)
       expect(s.lat.toFixed(6)).toBe(lat)
       expect(s.lng.toFixed(6)).toBe(lng)
-
-      dom.window.close()
     })
   })
 
   describe("各路線の登録駅を確認", () => {
-    test.each(lines)("%s", async (line) => {
+    test.concurrent.each(lines)("$name code:$code html:$ekimemo", async (line) => {
       // 駅メモ登録駅のみ検査対象
       const detail = readJsonSafe(`src/line/${line.code}.json`, jsonLine)
       const list_json = detail.station_list.filter(s => set.has(s.name) && !s.extra).map(s => s.name)
 
-      // https://github.com/jsdom/jsdom/issues/2583#issuecomment-559520814
-      const dom = await JSDOM.fromFile(`src/ekimemo/line/${line.ekimemo}.html`)
-      const list_csv = Array.from(dom.window.document.querySelectorAll(".line-station-list > .line-station-item"))
-        .map(item => item.querySelector(".station-name")?.textContent!!)
-      dom.window.close()
+      const html = (await readFile(`src/ekimemo/line/${line.ekimemo}.html`)).toString()
+      const m = html.match(/<ol class="line-station-list">.+?<\/ol>/ms)
+      if (!m) throw Error("station list not found ")
+      const list = Array.from(m[0].matchAll(/<li class="line-station-item">.+?<div class="station-name">(?<name>.+?)<\/div>.+?<\/li>/msg))
+      const list_csv = list.map(item => item?.groups?.["name"]!!)
 
       expect(list_json).toEqual(list_csv)
     })
