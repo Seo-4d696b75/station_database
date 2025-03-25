aded8ecfedc9e5e57f77e5eacb92834e65c93b77
Seo-4d696b75 <s.kaoru509@gmail.com>
2025-03-25 20:32:22 +0900

    fix: データセットに変更が無い場合も考慮する

--- src/ts/version.test.ts
+++ src/ts/version.test.ts
@@ -1,9 +1,10 @@
 import { parse } from 'dotenv'
-import { readFileSync } from 'fs'
+import { readFile } from 'fs/promises'
+import * as crypto from 'crypto'
 
-test('バージョン更新の確認', () => {
-  const oldEnv = parse(readFileSync('artifact/.env'))
-  const newEnv = parse(readFileSync('src/.env'))
+test('バージョン更新の確認', async () => {
+  const oldEnv = parse(await readFile('artifact/.env'))
+  const newEnv = parse(await readFile('src/.env'))
 
   const oldVersion = parseInt(oldEnv.VERSION || '0', 10)
   if (!oldVersion || oldVersion < 1000_00_00) {
@@ -15,6 +16,21 @@ test('バージョン更新の確認', () => {
     throw new Error('新バージョンが不明です')
   }
 
-  expect(newVersion).toBeGreaterThan(oldVersion)
-  console.log(`バージョン ${oldVersion} > ${newVersion}`)
+  const oldMainHash = await md5('artifact/main.zip')
+  const oldExtraHash = await md5('artifact/extra.zip')
+  const newMainHash = await md5('out/main/json.zip')
+  const newExtraHash = await md5('out/extra/json.zip')
+  if (oldMainHash === newMainHash && oldExtraHash === newExtraHash) {
+    expect(newVersion).toBe(oldVersion)
+    console.log(`バージョン ${oldVersion} (データ変更なし)`)
+  } else {
+    expect(newVersion).toBeGreaterThan(oldVersion)
+    console.log(`バージョン ${oldVersion} > ${newVersion}`)
+  }
 })
+
+async function md5(path: string): Promise<string> {
+  const md5 = crypto.createHash('md5')
+  const bin = await readFile(path)
+  return md5.update(bin).digest('hex')
+}
