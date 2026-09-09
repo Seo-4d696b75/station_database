fbd08bf275fb4baffec79568ee7b5ff5e2d65c4d
Seo-4d696b75 <s.kaoru509@gmail.com>
2026-09-09 22:05:26 +0900

    feat: 図形計算のライブラリ更新
    エントリー関数を独自実装に変更
    kotlin jvm 追加

--- src/diagram/build.gradle.kts
+++ src/diagram/build.gradle.kts
@@ -1,37 +1,16 @@
-import java.io.FileInputStream
-import java.util.Properties
-
 plugins {
-    id("java")
     id("application")
+    kotlin("jvm") version "2.4.20"
 }
 
 application {
-    mainClass.set("jp.seo.station.app.MainKt")
-}
-
-val props = Properties().apply {
-    val file = project.file("credentials.properties")
-    if (file.exists()) {
-        file.inputStream().use {
-            load(it)
-        }
-    }
+    mainClass.set("MainKt")
 }
 
 repositories {
-    maven {
-        name = "GitHubPackages"
-        url = uri("https://maven.pkg.github.com/Seo-4d696b75/diagram")
-        credentials {
-            // read .properties file in local, or env variables in GitHub Action
-            username = props.getProperty("username") ?: System.getenv("GITHUB_PACKAGE_USERNAME")
-            password = props.getProperty("token") ?: System.getenv("GITHUB_PACKAGE_TOKEN")
-        }
-    }
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.github.seo4d696b75:diagram:0.2.2")
+    implementation("com.seo4d696b75.diagram:station:0.3.0")
 }
